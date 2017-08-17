3bd09612925e075b4dee990fe66fc0cddf491cb7
Otso Jousimaa <otso@ojousima.net>
2017-08-17 19:45:49 +0000

    MAM parsing, plaintext handling done

--- handlers/mam.js
+++ handlers/mam.js
@@ -2,10 +2,32 @@ var MAM_payload = "";
 var MAM_root    = "";
 var chuncks     = [];
 
+/* https://gist.github.com/taterbase/2784890 */
+function bin2string(array){
+	var result = "";
+	for(var i = 0; i < array.length; ++i){
+		result += (String.fromCharCode(array[i]));
+	}
+	return result;
+}
+
 module.exports = function(request) {
   chuncks[request.index] = request.payload;
-  if(request.index == 147){
-    console.log("MAM: %s", chuncks.join("");
+
+  if(request.index == 152 && chuncks.length == 153){
+    let ii = 0;
+    let message = "";
+    let root = "";
+    for(ii = 0; ii < 148; ii++){
+      message += bin2string(chuncks[ii]);
+    }
+    console.log(message);
+    
+    for( ; ii < 153; ii++){
+      root += bin2string(chuncks[ii]);
+    }
+    console.log(root);
   }
+  //console.log("%d, %d", request.index, chuncks.length)
   return "ok"
 };
--- handlers/plaintext.js
+++ handlers/plaintext.js
@@ -1,4 +1,13 @@
+/* https://stackoverflow.com/questions/3195865/converting-byte-array-to-string-in-javascript */
+function bin2String(array) {
+  var result = "";
+  for (var i = 0; i < array.length; i++) {
+    result += String.fromCharCode(array[i]);
+  }
+  return result;
+}
+
 module.exports = function(request) {
-  console.log(request.payload);
+  console.log(bin2String(request.payload));
   return "ok"
 };
